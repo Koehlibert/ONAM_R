cf5d7d884353cf268cc246917529dd0dd25d189c
david_koehler <koehler@imbie.uni-bonn.de>
2025-02-24 15:08:10 +0100

    Added S3 functionality, restructured function files

---
+++ R/onam.R
@@ -0,0 +1,166 @@
+#' Fit orthogonal neural additive model
+#'
+#' @param formula Formula for model fitting. Specify deep parts with the same
+#' name as `list_of_deep_models`.
+#' @param list_of_deep_models List of named models used in `model_formula`.
+#' @param data Data to be fitted
+#' @param categorical_features Vector of which features are categorical.
+#' @param epochs Number of epochs to train the model.
+#' @param n_ensemble Number of orthogonal neural additive model ensembles
+#' @param callback Callback to be called during training.
+#' @param progresstext Show model fitting progress. If `TRUE`, shows current
+#' number of ensemble being fitted
+#' @param verbose Verbose argument for internal model fitting. used for
+#' debugging.
+#' @returns Returns a pho model object, containing all ensemble members,
+#' ensemble weights, and main and interaction effect outputs.
+#' @examples
+#' \donttest{
+#' # Basic example for a simple ONAM-model
+#' # Create training data
+#' n <- 1000
+#' x1 <- runif(n, -2, 2)
+#' x2 <- runif(n, -2, 2)
+#' y <- sin(x1) + ifelse(x2 > 0, pweibull(x2, shape = 3),
+#'   pweibull(-x2, shape = 0.5)) +
+#'   x1 * x2
+#' data_train <- cbind(x1, x2, y)
+#' # Define model
+#' model_formula <- y ~ mod1(x1) + mod1(x2) +
+#'   mod1(x1, x2)
+#' list_of_deep_models <- list(mod1 = ONAM:::get_submodel)
+#' # Fit model
+#' callback <-
+#' keras::keras$callbacks$EarlyStopping(monitor = "loss",
+#'                                      patience = 10)
+#' mod <- fit_onam(model_formula, list_of_deep_models,
+#'                    data_train, n_ensemble = 2, epochs = 50,
+#'                    callback = callback,
+#'                    progresstext = TRUE, verbose = 1)
+#' }
+#' @export fit_onam
+fit_onam <- function(formula, list_of_deep_models,
+                     data, categorical_features = NULL,
+                     n_ensemble = 20,
+                     epochs = 500,
+                     callback = NULL,
+                     progresstext = FALSE, verbose = 0) {
+  model_info <-
+    get_theta(formula, list_of_deep_models)
+  data_fit <-
+    prepare_data(data, model_info, categorical_features)
+  cat_counts <- get_category_counts(categorical_features,
+                                    data)
+  y <- data[,which(colnames(data) ==
+                     as.character(model_info$outcome))]
+  ensemble <- list()
+  for(i in 1:n_ensemble) {
+    if (progresstext) {
+      show_progress(i, n_ensemble)
+    }
+    model_object <-
+      create_model(model_info, list_of_deep_models,
+                   categorical_features, cat_counts)
+    model_whole <- model_object$model
+    model_list <- model_object$model_list
+    #Fit model####
+    # callback <-
+    #   keras::keras$callbacks$EarlyStopping(monitor = "loss",
+    #                                        patience = 10)
+    history <- model_whole %>%
+      keras::fit(data_fit, y, epochs = epochs, callbacks = callback,
+                 verbose = verbose)
+    #Orthogonalize####
+    ensemble[[i]] <-
+      pho(model_list, model_info, data_fit)
+  }
+  model_list_pho <- list(ensemble = ensemble,
+                         model_info = model_info,
+                         data = data,
+                         categorical_features = categorical_features,
+                         input = model_whole$input)
+  data_model_eval <-
+    evaluate_model_pre(model_list_pho)
+  pho_ensemble_list <- pho_ensemble(data_model_eval, model_info)
+  w_post_ensemble <- pho_ensemble_list[[1]]
+  outputs_post_ensemble <- pho_ensemble_list[[2]]
+  out <- c(model_list_pho,
+           call = match.call(),
+           w_post_ensemble = list(w_post_ensemble),
+           outputs_post_ensemble = list(outputs_post_ensemble))
+  class(out) <- "onam"
+  out
+}
+#' Get summary of an onam object
+#'
+#' @description
+#' generates a summary of a fitted `onam` object including
+#' information on ensembling strategy and performance metrics such as
+#' correlation and degree of interpretabiltity
+#'
+#' @param object onam object fitted with `fit_onam` to be summarized
+#' @param ... further arguments passed to or from other methods.
+#' @returns Gives summary of onam object, including model inputs, number of
+#' ensembles, correlation of model output and original outcome variable, and
+#' interpretability metrics i_1 and i_2
+#' @examples
+#' \donttest{
+#' # Basic example for a simple ONAM-model
+#' # Create training data
+#' n <- 1000
+#' x1 <- runif(n, -2, 2)
+#' x2 <- runif(n, -2, 2)
+#' y <- sin(x1) + ifelse(x2 > 0, pweibull(x2, shape = 3),
+#'   pweibull(-x2, shape = 0.5)) +
+#'   x1 * x2
+#' data_train <- cbind(x1, x2, y)
+#' # Define model
+#' model_formula <- y ~ mod1(x1) + mod1(x2) +
+#'   mod1(x1, x2)
+#' list_of_deep_models <- list(mod1 = ONAM:::get_submodel)
+#' # Fit model
+#' callback <-
+#' keras::keras$callbacks$EarlyStopping(monitor = "loss",
+#'                                      patience = 10)
+#' mod <- fit_onam(model_formula, list_of_deep_models,
+#'                    data_train, n_ensemble = 2, epochs = 50,
+#'                    callback = callback,
+#'                    progresstext = TRUE, verbose = 1)
+#' summary(mod)
+#' }
+#' @method summary onam
+#' @export
+summary.onam <- function(object, ...) {
+  prediction <- rowSums(object$outputs_post_ensemble)
+  var_decomp <- diag(var(object$outputs_post_ensemble)) /
+    var(prediction)
+  res <- list(
+    call = object$call,
+    input = object$input,
+    n_ensemble = length(object$ensemble),
+    cor = cor(rowSums(object$outputs_post_ensemble),
+              object$data[,as.character(object$model_info$outcome)]),
+    i_1 = var_decomp[1],
+    i_2 = var_decomp[2],
+    degree_expl = sum(var_decomp[1:2])/sum(var_decomp))
+  class(res) <- "summary.onam"
+  res
+}
+
+#' @rdname summary.onam
+#' @method print summary.onam
+#' @export
+print.summary.onam <- function(x, ...) {
+  cat("\nCall:\n")
+  print(x$call)
+  cat("\nInputs: \n", x$inputs, sep = "")
+  cat("\nCorrelation of model prediction with outcome variable: ",
+      round(x$cor, 4), sep = "")
+  cat("\nNumber of ensemble members: ", x$n_ensemble)
+  cat("\nI_1: ", round(x$i_1, digits = 4), "; I_2:  ",
+      round(x$i_2, digits = 4), sep = "")
+  cat("\nDegree of interpretability: ",
+      round(x$degree_expl, digits = 4),
+      sep = "")
+  invisible(x)
+}
